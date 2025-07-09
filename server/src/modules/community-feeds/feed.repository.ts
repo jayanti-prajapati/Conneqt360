793ef3c015c6e6ff3a49ffbec851c430d5f2572e
cds-naresh <nareshprajapat7994@gmail.com>
2025-07-09 16:18:36 +0530

    fix: community feed update comment fixed

--- server/src/modules/community-feeds/feed.repository.ts
+++ server/src/modules/community-feeds/feed.repository.ts
@@ -14,4 +14,37 @@ export class CommunityRepository extends BaseRepository<IPost> {
   async getFeedByUserId(userId: string, extraFilter: any = {}) {
     return Community_Feeds.getFeedByUserId({ user: userId, ...extraFilter });
   }
+
+  async update(id: string, data: Partial<IPost>, condition: any) {
+  const updateData: any = {};
+  const directFields = ["content", "imageUrl", "videoUrl", "description", "share"];
+
+ 
+  for (const field of directFields) {
+    if (data[field as keyof IPost] !== undefined) {
+      updateData[field] = data[field as keyof IPost];
+    }
+  }
+
+ 
+  if (data.comments && Array.isArray(data.comments)) {
+    updateData.$push = {
+      comments: {
+        $each: data.comments.map((comment) => ({
+          user: comment.user,
+          content: comment.content,
+          createdAt: comment.createdAt || new Date(),
+        })),
+      },
+    };
+  }
+
+  return this.model
+    .findOneAndUpdate({ _id: id, ...condition }, updateData, { new: true })
+    .populate("user", "name username email phone businessName businessType profileUrl")
+    .populate("comments.user", "name username email phone businessName businessType profileUrl");
+}
+
+
+  
 }
